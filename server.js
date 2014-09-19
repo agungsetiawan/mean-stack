a48099ec2b2244fae5b3c668685b3f079198496a
Agung Setiawan <com.agungsetiawan@gmail.com>
2014-09-19 15:03:37 +0700

    use path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
 var comments=require('./routes/comments');
+var path=require('path');
 
 mongoose.connect('mongodb://localhost:27017/mean');
 
@@ -14,7 +15,8 @@ app.use(express.static(__dirname+'/public'));
 app.use('/api',comments);
 
 app.get('*', function(req, res) {
-		res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		// res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendFile('index.html',{root:path.join(__dirname,'/public')});
 	});
 
 app.listen(8181);
